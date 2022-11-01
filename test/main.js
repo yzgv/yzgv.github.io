bc0ed94a2957a83b9b807ab1417a59e2f06ba0d8
yzgv <leojobs@qq.com>
2022-11-01 17:11:21 +0800

    Signed-off-by: yzgv <leojobs@qq.com>

--- test/main.js
+++ test/main.js
@@ -7,7 +7,3 @@ function start() {
 function end() {
 	clearTimeout(timer);
 }
-
-var date = new Date(),
-		week = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期天"];
-document.write(date.toLocaleString() + week[date.getDay()] + "<br/>");
