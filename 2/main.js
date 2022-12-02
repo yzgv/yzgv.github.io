4e9547ff0eb2e86f5131def9186086fa139caf95
yzgv <leojobs@qq.com>
2022-12-02 16:53:39 +0800

    Signed-off-by: yzgv <leojobs@qq.com>

--- 2/main.js
+++ 2/main.js
@@ -1,45 +1,45 @@
-var msg = document.querySelector('#msg');
-var send = document.querySelector('#send');
-var copy = document.querySelector('#copy');
-var recall = document.querySelector('#recall');
-var del = document.querySelector('#delete');
-var msgs = document.querySelector('#msgs');
-var cpys = document.querySelectorAll('.cpy');
-var dels = document.querySelectorAll('.del');
+var wow = document.querySelector('.window');
+var msg = document.querySelector('.msg');
+var send = document.querySelector('.send');
+var copy = document.querySelector('.copy');
+var recall = document.querySelector('.recall');
+var msgs = document.querySelector('.msgs');
+var cpys = document.getElementsByClassName('cpy');
+var rcls = document.getElementsByClassName('rcl');
+
+wow.style.height = (document.body.clientHeight - 48 - 92) + 'px';
+
 send.onclick = function() {
 	if(msg.value == '') {
 		alert('输入为空');
 	} else {
 		var li = document.createElement('li');
-		var span = document.createElement('span');
-		var cpy = document.createElement('a');
-		var del = document.createElement('a');
-		span.innerHTML = msg.value;
-		cpy.setAttribute('href', 'javascript:;');
+		var txt = document.createElement('span');
+		var cpy = document.createElement('span');
+		var rcl = document.createElement('span');
+		txt.innerHTML = msg.value;
+		txt.setAttribute('class', 'txt');
 		cpy.setAttribute('class', 'cpy');
-		cpy.innerText = '复制';
-		del.setAttribute('href', 'javascript:;');
-		del.setAttribute('class', 'del');
-		del.innerText = '删除';
-		li.appendChild(del);
+		rcl.setAttribute('class', 'rcl');
+		li.appendChild(rcl);
 		li.appendChild(cpy);
-		li.appendChild(span);
-		if(msgs.children.length <= 1) {
+		li.appendChild(txt);
+		if(msgs.children.length <= 0) {
 			msgs.insertBefore(li, msgs.children[0]);
 		} else {
-			msgs.insertBefore(li, msgs.children[msgs.children.length - 1]);
+			msgs.insertBefore(li, msgs.children[msgs.children.length]);
 		}
 	}
 }
+
 recall.onclick = function() {
+	rcls = document.getElementsByClassName('rcl');
+	for(var i = 0; i < rcls.length; i++) {
+		rcls[i].classList.add("display");
+	}
 	if(msgs.children.length <= 0) {
 		alert('消息为空');
 	} else {
 		msgs.removeChild(msgs.children[msgs.children.length - 1]);
-	}
-}
-for(var i = 0; i < dels.length; i++) {
-	dels[i].onclick = function() {
-		msgs.removeChild(this.parentNode);
 	}
 }
